b89d15b756f11b59ebcadc0af35449555b5e907a
joeng03 <51855281+joeng03@users.noreply.github.com>
2020-06-28 15:40:51 +0800

    Update scraper.py

--- myapp/scraper.py
+++ myapp/scraper.py
@@ -8,7 +8,19 @@ def write_to_csv(file,rows_list):
   messages = soup.select('div.uiBoxWhite')
   for message in messages:
       d={}
-      d["Info"]=message.select('div')[4].text
+      split=0
+      msgs=message.select('div')[4].text
+      msg=[]
+      for i in range(0,len(msgs)):
+        if(msgs[i]==','):
+          msg.append(msgs[split:i])
+          split=i+1
+      try:
+        d["Name"]=msg[0]
+        d["Phone Number"]=msg[1]
+        d["Temperature"]=msg[2]
+      except IndexError:
+        pass
       d["Timestamp"]=message.select('div')[7].text
       if not d["Info"].startswith('Say hi to your new Facebook friend'):
         rows_list.append(d)
